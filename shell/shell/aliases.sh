3703420b66d524b9159f59f98c65c9a35ede9579
Sukrat Kashyap <kashyapsukrat@gmail.com>
2024-04-01 14:39:32 +0100

    Adding desk-upgrade alias

--- shell/shell/aliases.sh
+++ shell/shell/aliases.sh
@@ -1,5 +1,6 @@
 alias reload="omz reload"
 alias ghub="github"
 alias rm="rm -v"
-alias egrep="grep -E"
-alias esed="sed -r"
+alias grep="grep -E"
+alias sed="sed -E"
+alias desk-upgrade="sudo apt-get update && sudo apt-get upgrade -y && snap refresh"
