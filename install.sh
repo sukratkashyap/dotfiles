eb0b3262172402d8628e70a9b1439de1fd6b739a
Sukrat Kashyap <kashyapsukrat@gmail.com>
2025-04-29 21:49:56 +0100

    fix: -h incompatibility with linux

--- install.sh
+++ install.sh
@@ -21,11 +21,11 @@ function run-configure() {
 
 function configure-shell() {
     echo "- Configuring shell"
-    ln -sfvh "$(pwd)/shell/zshrc" ~/.zshrc
-    ln -sfvh "$(pwd)/shell/zsh_theme" ~/.zsh_theme
-    ln -sfvh "$(pwd)/shell/bashrc" ~/.bashrc
+    ln -sfnv "$(pwd)/shell/zshrc" ~/.zshrc
+    ln -sfnv "$(pwd)/shell/zsh_theme" ~/.zsh_theme
+    ln -sfnv "$(pwd)/shell/bashrc" ~/.bashrc
     mkdir -pv ~/.shell
-    ln -sfvh "$(pwd)/shell/shell/"* ~/.shell
+    ln -sfnv "$(pwd)/shell/shell/"* ~/.shell
 }
 
 function configure-git() {
@@ -38,34 +38,34 @@ function configure-git() {
 function configure-ssh() {
     echo "- Configuring SSH"
     mkdir -pv ~/.ssh/config.d
-    ln -sfvh "$(pwd)/ssh/config" ~/.ssh/config
-    ln -sfvh "$(pwd)/ssh/config.d/"* ~/.ssh/config.d
+    ln -sfnv "$(pwd)/ssh/config" ~/.ssh/config
+    ln -sfnv "$(pwd)/ssh/config.d/"* ~/.ssh/config.d
     echo "-- Setting permissions"
     chmod -v -R 700 ~/.ssh
 }
 
 function configure-tmux() {
     echo "- Configuring tmux"
-    ln -sfvh "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
+    ln -sfnv "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
 }
 
 function configure-vscode() {
     echo "- Configuring vscode"
     if is-macos; then
-        ln -sfvh "$(pwd)/configs/vscode/keybindings-macos.json" ~/"Library/Application Support/Code/User/keybindings.json"
-        ln -sfvh "$(pwd)/configs/vscode/settings.json" ~/"Library/Application Support/Code/User/settings.json"
+        ln -sfnv "$(pwd)/configs/vscode/keybindings-macos.json" ~/"Library/Application Support/Code/User/keybindings.json"
+        ln -sfnv "$(pwd)/configs/vscode/settings.json" ~/"Library/Application Support/Code/User/settings.json"
     else
-        ln -sfvh "$(pwd)/configs/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
-        ln -sfvh "$(pwd)/configs/vscode/settings.json" ~/.config/Code/User/settings.json
+        ln -sfnv "$(pwd)/configs/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
+        ln -sfnv "$(pwd)/configs/vscode/settings.json" ~/.config/Code/User/settings.json
     fi
 }
 
 function configure-tabby() {
     echo "- Configure tabby"
     if is-macos; then
-        ln -sfvh "$(pwd)/configs/tabby/config-macos.yaml" ~/"Library/Application Support/tabby/config.yaml"
+        ln -sfnv "$(pwd)/configs/tabby/config-macos.yaml" ~/"Library/Application Support/tabby/config.yaml"
     else
-        ln -sfvh "$(pwd)/configs/tabby/config-linux.yaml" ~/.config/tabby/config.yaml
+        ln -sfnv "$(pwd)/configs/tabby/config-linux.yaml" ~/.config/tabby/config.yaml
     fi
 }
 
@@ -73,7 +73,7 @@ function configure-karabiner() {
     echo "- Configure karabiner"
     if is-macos; then
         mkdir -pv ~/.config
-        ln -sfvh "$(pwd)/configs/karabiner" ~/.config/karabiner
+        ln -sfnv "$(pwd)/configs/karabiner" ~/.config/karabiner
     fi
 }
 
