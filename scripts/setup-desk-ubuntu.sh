7c2631a29a304ff8ee06cc7f385d138a237c4bf3
Sukrat Kashyap <kashyapsukrat@gmail.com>
2025-04-30 22:12:36 +0100

    Create setup-desk-ubuntu.sh

---
+++ scripts/setup-desk-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "- Installing common packages"
+sudo apt-get update
+sudo apt-get install -y \
+    wget curl \
+    apt-transport-https ca-certificates \
+    gnupg-agent software-properties-common \
+    gcc g++ make build-essential \
+    zsh tmux vim
+
+echo "- Set ZSH as default shell"
+chsh -s $(which zsh)
+
+echo "- Installing DEV packages"
+sudo apt-get install -y \
+    git \
+    python3 python3-dev python3-pip \
+    default-jdk
