3850c2a40bd10b56d232815bf9a457b09acd644a
Sukrat Kashyap <kashyapsukrat@gmail.com>
2025-04-30 22:05:43 +0100

    Update setup-common.sh

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -2,9 +2,18 @@
 
 set -e
 
-# Install ohmyzsh
-export ZSH=${ZSH:-~/.oh-my-zsh}
-export ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-test -d ${ZSH} || git clone https://github.com/ohmyzsh/ohmyzsh.git ${ZSH}
-test -d ${ZSH_CUSTOM}/plugins/zsh-autosuggestions || git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-test -d ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+export ZSH=~/.oh-my-zsh
+export ZSH_PLUGINS="${ZSH}/custom/plugins"
+
+if test ! -d "${ZSH}"; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ${ZSH}
+fi
+
+mkdir -p "${ZSH_PLUGINS}"
+if test ! -d "${ZSH_PLUGINS}/zsh-autosuggestions"; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_PLUGINS}/zsh-autosuggestions
+fi
+
+if test ! -d "${ZSH_PLUGINS}/zsh-syntax-highlighting"; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_PLUGINS}/zsh-syntax-highlighting
+fi
